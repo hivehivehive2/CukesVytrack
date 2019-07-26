36362b9e5ce6404416b3794e97826a6cc8886d4a
hakuban <hakuban@yahoo.com>
2019-07-26 18:22:31 -0400

    added main method in runner

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -3,26 +3,29 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin={"html:target/reports","rerun:target/Failed/rerun.txt"},
-//        features = "src/main/resources/Activities.feature"
-       features = "src/main/resources/Customers.feature:8"
-//        features = "src/main/resources/Customers.feature:28"
-//        features = "src/main/resources/DAshboards.feature"
-//        features = "src/main/resources/Fleet.feature"
-//        features = "src/main/resources/Login_Navigation.feature:32"
-//        features = "src/main/resources/Login_Navigation.feature:44"
-//        features = "src/main/resources/Marketing.feature"
-//        features = "src/main/resources/Reports_And_Segments.feature"
-//        features = "src/main/resources/Sales.feature"
-//        features = "src/main/resources/System.feature"
-
-        //features = "src/main/resources"
-        //,glue={"src/main/"}
-        //   ,tags= {"@smoke"}
-)
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//        plugin={"html:target/reports","rerun:target/Failed/rerun.txt"},
+////        features = "src/main/resources/Activities.feature"
+//       features = "src/main/resources/Customers.feature:8"
+////        features = "src/main/resources/Customers.feature:28"
+////        features = "src/main/resources/DAshboards.feature"
+////        features = "src/main/resources/Fleet.feature"
+////        features = "src/main/resources/Login_Navigation.feature:32"
+////        features = "src/main/resources/Login_Navigation.feature:44"
+////        features = "src/main/resources/Marketing.feature"
+////        features = "src/main/resources/Reports_And_Segments.feature"
+////        features = "src/main/resources/Sales.feature"
+////        features = "src/main/resources/System.feature"
+//
+//        //features = "src/main/resources"
+//        ,glue={"Mystepdefs"}
+//        //   ,tags= {"@smoke"}
+//)
 
 public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Testing");
+    }
 
 }
