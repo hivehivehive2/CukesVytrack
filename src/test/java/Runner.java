a8846e081665e35217d786af5b8ead12c84b0c41
hakuban <hakuban@yahoo.com>
2019-07-26 17:40:13 -0400

    deleted target

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -7,12 +7,12 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
         plugin={"html:target/reports","rerun:target/Failed/rerun.txt"},
 //        features = "src/main/resources/Activities.feature"
-//       features = "src/main/resources/Customers.feature:8"
+       features = "src/main/resources/Customers.feature:8"
 //        features = "src/main/resources/Customers.feature:28"
 //        features = "src/main/resources/DAshboards.feature"
 //        features = "src/main/resources/Fleet.feature"
 //        features = "src/main/resources/Login_Navigation.feature:32"
-        features = "src/main/resources/Login_Navigation.feature:44"
+//        features = "src/main/resources/Login_Navigation.feature:44"
 //        features = "src/main/resources/Marketing.feature"
 //        features = "src/main/resources/Reports_And_Segments.feature"
 //        features = "src/main/resources/Sales.feature"
